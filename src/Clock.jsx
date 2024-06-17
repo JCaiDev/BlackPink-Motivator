563c8aacf0b3d497f79a37c273b975111908eace
Jackie Dai <jackydai12.21@gmail.com>
2024-06-17 17:31:23 -0400

    added working clock

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,15 +1,24 @@
-export default function Clock({ time }) {
-    const hours = time.getHours()
-    const className = hours > 21 || hours <= 6 ? 'night' : 'day';
-    
-    function formateDate(date) {
-        return date.toLocaleDateString('en-US',
-            { weekday: 'long' })
-    }
+import React, { useState, useEffect } from 'react';
 
-    return (
-        <h1 id ="time" className={className} >
-            Today is {formateDate(time)}, {time.toLocaleTimeString()}
-        </h1>
-    )
+
+export default function Clock() {
+  const [time, setTime] = useState(new Date());
+  const hours = time.getHours();
+  const className = hours > 21 || hours <= 6 ? 'night' : 'day';
+
+  function formateDate(date) {
+    return date.toLocaleDateString('en-US', { weekday: 'long' });
+  }
+
+  // updates the time every second
+  useEffect(() => {
+    const intervalId = setInterval(() => setTime(new Date()), 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <h1 id="time" className={className}>
+      Today is {formateDate(time)}, {time.toLocaleTimeString()}
+    </h1>
+  );
 }
